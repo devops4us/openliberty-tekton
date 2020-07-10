72834cdd96fafc02f53c45d0cf50be67f4c77354
gottfried.luef <gottfried.luef@at.ibm.com>
2020-07-10 14:39:52 +0200

    test

--- run-triggers.bat
+++ run-triggers.bat
@@ -6,4 +6,4 @@
 kubectl delete  -f ci-cd-pipeline/tekton-triggers/webhook-service-account.yaml  -n env-ci
 kubectl create  -f ci-cd-pipeline/tekton-triggers/webhook-service-account.yaml  -n env-ci
 kubectl delete -f ci-cd-pipeline/tekton-triggers/webhook-event-listener-kubernetes.yaml -n env-ci 
-kubectl create -f ci-cd-pipeline/tekton-triggers/webhook-event-listener-kubernetes.yaml -n env-ci  
+kubectl create -f ci-cd-pipeline/tekton-triggers/webhook-event-listener-kubernetes.yaml -n env-ci     
